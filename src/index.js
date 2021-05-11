7204ccdc568fcc05d2c49cc1b08baf0538f3391f
Mehfooz-ur-Rehman <mehfoozijaz786@gmail.com>
2021-05-11 13:08:06 +0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const { app, BrowserWindow, ipcRenderer } = require('electron');
-const { ipcMain } = require('electron/main');
-const path = require('path');
+const { app, BrowserWindow, ipcRenderer } = require("electron");
+const { ipcMain } = require("electron/main");
+const path = require("path");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
   app.quit();
 }
 
@@ -13,15 +14,15 @@ const createWindow = () => {
     minWidth: 1000,
     minHeight: 700,
     frame: false,
-    backgroundColor: '#282a36',
+    backgroundColor: "#282a36",
     webPreferences: {
-      nodeIntegration: true, 
-      enableRemoteModule: true
-    }
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools();
@@ -30,39 +31,21 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
-function aboutWindow(){
-  // Create the browser window.
-  const aboutwindow = new BrowserWindow({
-    minWidth: 300,
-    minHeight: 200,
-    frame: false,
-    backgroundColor: '#282a36',
-    webPreferences: {
-      nodeIntegration: true, 
-      enableRemoteModule: true
-    }
-})
-}
-
-ipcRenderer.send.aboutWindow();
